a6820d0f68dc55b882af070075e6154dd7dce8dd
mattchew160 <31747778+mattchew160@users.noreply.github.com>
2021-10-18 15:43:27 -0400

    testing js

--- testingJS.js
+++ testingJS.js
@@ -1,7 +1,14 @@
 let total_meeps = 0;
+
+$('#plus1').click(function(){
+
+
+
+
+total_meeps++;
 console.log(total_meeps);
-total_meeps = chrome.storage.sync.get('total_meeps');
-console.log(total_meeps);
+
+$('#total_mee').val(total_meeps);
 
 
-
+});
