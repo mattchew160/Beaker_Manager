ab8112056e8ce942f9bde9e6b5ff615eba6f661f
mattchew160 <31747778+mattchew160@users.noreply.github.com>
2021-10-21 21:09:01 -0400

    good work
    Got the meeps to update when I click on the button from the popup, need to work on the audio again. made progress tho and i'm happy with that.

--- popup.js
+++ popup.js
@@ -1,83 +1,75 @@
-$(function(){
-
-    chrome.storage.sync.get(['total_meeps', 'limit'],function(beaker){
-        $('#total_meeps').val(beaker.total);
-        $('#limit').val(beaker.limit);
-
-        
-    });
-
- 
-
-    $('#spent_amount').click(function(){
-        chrome.storage.sync.get('total', function(beaker) {
-            var newTotal = 0;
-            if (beaker.total){
-                newTotal += parseInt(beaker.newTotal);
-            }
-
-            var amount = $('#amount').val();
-            if(amount){
-                newTotal += parseInt(amount);
-
-            }
-            chrome.storage.sync.set({'total': newTotal});
-
-            $('#total').text(newTotal);
-            
-            $('#amount').val('');
-        });
 
 
 
 
-    });
 
 
+// runs js only after the Dom fully loads, this is a must!!!!!
+window.onload=function(){
 
-    $('#on_click').click(function(){
-        
-        let x =Math.floor( Math.random() * 4);
-        
-        switch(x) {
-            case 0:
-                audio = new Audio('audio/mee_1.mp3');
-                audio.play();
-                
-                break;
-
-            case 1:
-                audio =  new Audio('audio/mee_2.mp3');
-                audio.play();
-                break;
-
-            case 2:
-                audio = new Audio('audio/mee_3.mp3');
-                audio.play();
-                break;
-
-            case 3:
-                audio = new Audio('audio/mee_4.mp3');
-                audio.play();
-                break;
-
-
-             
-        }
-
-        
-        chrome.storage.sync.set({'total_meeps': (total_meeps)}, function(){
-            
-        
-        });
-        console.log(total_meeps);
-       
-        
-
-
-    });
-
+    let meeps = 0;
+    let on_off = true;
     
-});
+    // type conversion  let meeps = String(total_meeps);
+    
+    /// this is very important for developing chrome extentions, chrome will 
+    // not run inline script, it must be in a seperate js file
+    // and then needs this line here, where chrome needs an event 
+    // listener to allow a script will a function to happen. 
+    // plus is the name of the function and the second plus is adding the event 
+    // listener to the plus function
+    
+    document.getElementById("plus").addEventListener('click', plus1);
+    document.getElementById("meep_noise").addEventListener('click', on_click_noise);
+    
+    
+    function plus1() {
+        let mes = Number(document.getElementById('total_meeps').innerHTML);
+        mes++;
+        document.getElementById('total_meeps').innerHTML = String(mes);
+        
+    
+    
+    
+
+}
+
+function on_click_noise(){
+                
+    let x =Math.floor( Math.random() * 4);
+    
+    switch(x) {
+        case 0:
+            audio = new Audio('audio/mee_1.mp3');
+            audio.play();
+            
+            break;
+
+        case 1:
+            audio =  new Audio('audio/mee_2.mp3');
+            audio.play();
+            break;
+
+        case 2:
+            audio = new Audio('audio/mee_3.mp3');
+            audio.play();
+            break;
+
+        case 3:
+            audio = new Audio('audio/mee_4.mp3');
+            audio.play();
+            break;
 
 
+         
+    }
+
+
+}
+
+
+
+// == loose comparison
+
+//=== stict comparison, values must be of the same data type to be compared
+} // on window load
--- testingJS.js
+++ testingJS.js
@@ -1,14 +1,39 @@
-let total_meeps = 0;
 
-$('#plus1').click(function(){
+// runs js only after the Dom fully loads, this is a must!!!!!
+window.onload=function(){
+
+    let meeps = 0;
+    let on_off = true;
+    
+    // type conversion  let meeps = String(total_meeps);
+    
+    /// this is very important for developing chrome extentions, chrome will 
+    // not run inline script, it must be in a seperate js file
+    // and then needs this line here, where chrome needs an event 
+    // listener to allow a script will a function to happen. 
+    // plus is the name of the function and the second plus is adding the event 
+    // listener to the plus function
+    
+    document.getElementById("plus").addEventListener('click', plus1);
+    
+    
+    
+    function plus1() {
+        let mes = Number(document.getElementById('total_meeps').innerHTML);
+        mes++;
+        document.getElementById('total_meeps').innerHTML = String(mes);
+        }
+    
+    
+
+
+}
 
 
 
 
-total_meeps++;
-console.log(total_meeps);
-
-$('#total_mee').val(total_meeps);
 
 
-});
+// == loose comparison
+
+//=== stict comparison, values must be of the same data type to be compared
